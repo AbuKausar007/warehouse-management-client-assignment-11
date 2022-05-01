cd4737927b472cb609fe8846f9427ec276b53f4e
AbuKausar007 <a.25kausar@gmail.com>
2022-05-02 01:41:56 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from "./logo.svg";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/LandingPage/Home/Home";
+import Inventory from "./pages/LandingPage/Inventory/Inventory";
+import Header from "./pages/Shared/Header/Header";
+import Login from "./pages/SignUp/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <h1>Warehouse Website</h1>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/inventory" element={<Inventory></Inventory>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
