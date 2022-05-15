0142357a84c794870d8de8dc6a0bb4adf33a4dc4
AbuKausar007 <a.25kausar@gmail.com>
2022-05-15 12:08:32 +0600

    delete item done

--- src/pages/ManageInventories/ManageInventories.js
+++ src/pages/ManageInventories/ManageInventories.js
@@ -1,15 +1,62 @@
 import React from "react";
 import useInventory from "../hooks/useInventory/useInventory";
-import InventoryTable from "../InventoryTable/InventoryTable";
+import Table from "react-bootstrap/Table";
 
 const ManageInventories = () => {
-  const { items } = useInventory();
+  const { items, setItems } = useInventory();
+  const handleDelete = (id) => {
+    const proceed = window.confirm("Are you sure?");
+    if (proceed) {
+      const url = `http://localhost:5000/inventories/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          const remaining = items.filter((item) => item._id !== id);
+          setItems(remaining);
+        });
+    }
+  };
   return (
     <div className="py-5 mt-3">
       <h1 className="fw-bold text-success">Manage All Inventories</h1>
       <section>
         {items.map((item) => (
-          <InventoryTable key={item._id} item={item}></InventoryTable>
+          <div key={item._id}>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Image</th>
+                  <th>supplier</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="text-center">
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
+                  <td>{item.quantity}</td>
+                  <td>
+                    <img src={item.img} alt="" width={100} />
+                  </td>
+                  <td>{item.supplier}</td>
+                  <td>
+                    <button
+                      onClick={() => handleDelete(item._id)}
+                      className="btn btn-danger fw-bold"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
         ))}
       </section>
     </div>
